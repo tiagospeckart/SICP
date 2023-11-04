692f2a30ee7ac8755002a4ce7f5075214262e857
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-11-03 21:47:33 -0300

    exercise 1.3

--- 1.3. exercise.rkt
+++ 1.3. exercise.rkt
@@ -4,11 +4,19 @@
 ;arguments x y z
 ;define square
 ;define two biggest numbers
-;if x > y or x > z -> x
 ;if y > x or y > z -> y
 ;else z
 (define (square x) (* x x))
 
-(define (biggest-two-of-three x y z) 
-  ((and (>= x y) (>= x z)) ; x is one of the two biggest
-))
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+(define (biggest-two-of-three x y z) ; x is one of the two biggest
+(cond ((and (>= x y) (>= x z)) 
+          (+ (square x) (square (if (>= y z) y z))))
+      ((and (>= y x) (>= y z)) 
+          (+ (square y) (square (if (>= z x) z x))))
+      (else (+ (square z) (square (if (>= x y) x y))))
+      ))
+
+(biggest-two-of-three 11 5 10)
