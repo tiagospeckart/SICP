da8795b7a9f744559c982a61e4cb7d1aaf19fc86
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-11-03 21:54:22 -0300

    exercise 1.4

--- 1.4. exercise.rkt
+++ 1.4. exercise.rkt
@@ -4,4 +4,8 @@
 ;Use this observation to describe the behavior of the following procedure:
 
 (define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
+  ((if (> b 0) + -) a b)) 
+  ;if b is bigger than 0, the operation is a sum, otherwise, a subtraction
+  ;Defining operators through the arguments of the procedures is a insteresting way do change de behaviour of the procedure
+
+(a-plus-abs-b -21 -7)
